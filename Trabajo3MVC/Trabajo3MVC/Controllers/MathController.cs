3a5e6b5b456282a6cf93d2c936e7d66626484e24
JoseSH17 <josesh17@gmail.com>
2019-04-21 12:30:48 -0600

    Code for Work #3
    Code for Work #3 Complete

---
+++ Trabajo3MVC/Trabajo3MVC/Controllers/MathController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trabajo3MVC.Models;
+
+namespace Trabajo3MVC.Controllers
+{
+    public class MathController : Controller
+    {
+        // GET: Math
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult Result(MyMath myMath)
+        {
+            MyMath theResult = new MyMath();
+            theResult.NumberOne = myMath.NumberOne;
+            theResult.NumberTwo = myMath.NumberTwo;
+            theResult.Multiply = myMath.Multiply;
+            if (myMath.Multiply)
+                theResult.Result = myMath.NumberOne * myMath.NumberTwo;
+            else
+                theResult.Result = myMath.NumberOne + myMath.NumberTwo;
+            return View("Result", theResult);
+        }
+    }
+}
---
+++ Trabajo3MVC/Trabajo3MVC/Models/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trabajo3MVC.Models
+{
+    public partial class MyMath
+    {
+        public float NumberOne { get; set; }
+        public float NumberTwo { get; set; }
+        public bool Multiply { get; set; }
+        public float Result { get; set; }
+    }
+}
